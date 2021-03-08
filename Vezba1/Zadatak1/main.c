6380e68987cd36fcd20f9e1cbb7c809855f77289
SreckoVeselinovic <nzm993@gmail.com>
2021-03-08 18:15:24 +0100

    PrintResult function

--- Vezba1/Zadatak1/main.c
+++ Vezba1/Zadatak1/main.c
@@ -8,12 +8,18 @@ else
 return b - a;
 }
 
+void PrintResult(int res)
+{
+printf("Result: %d\n", res);
+}
+
+
 int main()
 {
 int a = 3;
 int b = 8;
 int c;
-Calculation (a, b);
-printf("Result: %d\n", c);
+Calculation (a,b);
+PrintResult(c);
 return 0;
 }
